d8d0c575a8504265c8b6b52a93a126d46e5a723c
IN1DM3B_06 <IN1DM3B_06@H015112.NET>
2019-09-17 09:51:24 +0200

    Hecho el lectorDatosPorTeclado

---
+++ Reto1ADW/src/test/java/Reto1ADW/TestLectorDatosPorTeclado.java
@@ -0,0 +1,14 @@
+package Reto1ADW;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestLectorDatosPorTeclado {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
